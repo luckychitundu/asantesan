b9ad3ceeeafbf3f7edd64ce7c1a0b00505bc400d
Lucky Chitundu <87881145+lchitundu@users.noreply.github.com>
2023-12-19 01:02:01 -0500

    updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,7 +30,7 @@ function App() {
 
     try {
       const dataToSend = { emailValue, selectedValue, customValue };
-      const response = await fetch('http://localhost:5000/api', {
+      const response = await fetch('https://asante-be.vercel.app/api', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
